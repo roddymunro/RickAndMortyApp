dd603b50de4aea821dda415fd28fcd9da28aa3a5
Roddy Munro <roddymunro@icloud.com>
2021-06-18 19:49:17 -0300

    Initial Commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Roddy Munro on 2021-06-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
