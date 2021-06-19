ad820cd2f1a1a5f2c5429ca5397be922462eaf0a
Roddy Munro <roddymunro@icloud.com>
2021-06-18 23:11:40 -0300

    Add useful UIView extensions for adding subviews and pinning view to edges

---
+++ RickAndMortyApp/Extensions/UIView+Ext.swift
@@ -0,0 +1,28 @@
+//
+//  UIView+Ext.swift
+//  RickAndMortyApp
+//
+//  Created by Roddy Munro on 2021-06-18.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addSubview(_ views: UIView...) {
+        for view in views {
+            addSubview(view)
+        }
+    }
+    
+    func pinToEdges(of superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superView.topAnchor),
+            bottomAnchor.constraint(equalTo: superView.bottomAnchor),
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor)
+        ])
+    }
+}
